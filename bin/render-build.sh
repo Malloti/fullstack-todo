e9b374366bee4393f121285b2d9bb3fb7e3fe396
Felipe Freitas <felipe96.ff@gmail.com>
2024-08-19 15:16:58 -0300

    add render scripts

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,10 +2,10 @@
 # exit on error
 set -o errexit
 
-bundle check || bundle install
+bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 
 bundle exec rails db:prepare
 
-yarn check --check-files || yarn install
+yarn install
